71fbbb57df1d0c6acb52f761cafb4f60f61a3bad
Iswarya <iswaryaishu985@gmail.com>
2024-02-08 12:55:12 -0500

    8-02-2023

---
+++ js/main.js
@@ -0,0 +1,41 @@
+function myfunction(){
+    var x = document.getElementById('myfunc');
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+function myfunction1(){
+    var x = document.getElementById('myfunc1');
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+function myfunction2(){
+    var x = document.getElementById('myfunc2');
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+function myfunction3(){
+    var x = document.getElementById('myfunc3');
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+
+// function playVedioFunction(){
+//     var y = document.getElementById("videoFrame");
+//     y.style.display =  "block";
+// }
